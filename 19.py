21e94495098f43562899c9095bdf741f3f2ecd85
Björn Andersson <bjorn.aaa@gmail.com>
2022-12-02 16:43:00 +0100

    Add gitignore for inputs, add 19

---
+++ 19.py
@@ -0,0 +1,42 @@
+#%%
+import collections as coll 
+import datetime as dt
+import itertools as it
+import math
+from operator import itemgetter as ig
+import pprint as pp
+import re
+from utils import advent
+#from grid import Grid
+
+samples = [
+""""""
+"""""",
+"""""",
+""""""
+]
+
+advent.setup(2020, 1)
+fin = advent.get_input()
+
+def solve(d):
+#   print("input: ", repr(d))
+    t=0
+#   nums = ints(d)
+#   ww = words(d)
+#   for line in ww:
+#     for w in line:
+
+    # inp = []
+    # out = []
+    
+    return t
+
+
+for sample in samples:
+    if (len(sample) > 0):
+        print(solve(sample))
+print("Answer")
+print(solve(fin.read()))
+        
+
